f6677155cac4caae7e1a0d90e98ad049e375c81b
Ajith Reddy <43786067+ajithreddygeth@users.noreply.github.com>
2021-08-09 09:07:35 +0530

    Update init.sh

--- ssl30days/init.sh
+++ ssl30days/init.sh
@@ -1,3 +1,8 @@
+# IF the insisnce has no profile attched to access S3 use the keys to do that 
+# $ export AWS_ACCESS_KEY_ID=AKIAI******
+# $ export AWS_SECRET_ACCESS_KEY=wJalrXUt**************
+# $ export AWS_DEFAULT_REGION=us-west-2
+
 (echo 'Hostname' && echo ',' && echo 'Expiery-date' && echo ',' && echo 'DNS-name') |tr '\n' '\t' >>expirey.csv
 aws s3 cp expirey.csv s3://ssl-report-test/$(date +%Y)/$(date +%m)/$(date +%d)/
 rm expirey.csv
