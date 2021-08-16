dac2e64fe225bba37c375a54c7923381ab4fa486
Ajith Reddy <43786067+ajithreddygeth@users.noreply.github.com>
2021-08-16 09:03:45 +0530

    Update findallcert.sh

--- ssl30days/findallcert.sh
+++ ssl30days/findallcert.sh
@@ -1,6 +1,5 @@
-#bin/bash
-export AWS_ACCESS_KEY_ID=AKIAV**********
-export AWS_SECRET_ACCESS_KEY=25GkPAP************
+export AWS_ACCESS_KEY_ID=AKIAVH********
+export AWS_SECRET_ACCESS_KEY=25GkP************
 export AWS_DEFAULT_REGION=us-west-2
 
 rm list.txt
@@ -19,5 +18,5 @@ echo "######"
 while read p; do
   echo >> expiry.csv && echo  $(hostname && echo ',' && echo `date --date="$(openssl x509 -in $p -noout -enddate | cut -d= -f 2)" --iso-8601` ; echo "," ;echo `openssl x509 -in $p -text | grep DNS` ; echo ","; echo "$p") |tr '\n' '\t' >> expiry.csv
 done <list.txt
-# aws s3 cp s3://ssl-cert-reports/$(date +%Y)/$(date +%m)/$(date +%d)/expiry.csv .
-# aws s3 cp expiry.csv s3://ssl-cert-reports/$(date +%Y)/$(date +%m)/$(date +%d)/
+
+aws s3 cp expiry.csv s3://ssl-cert-reports/$(date +%Y)/$(date +%m)/$(date +%d)/$(hostname)
