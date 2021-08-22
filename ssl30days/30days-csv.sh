0986f35110f8501357ea94b41383dade4b307944
Ajith Reddy <43786067+ajithreddygeth@users.noreply.github.com>
2021-08-22 19:06:14 +0530

    Update 30days-csv.sh

--- ssl30days/30days-csv.sh
+++ ssl30days/30days-csv.sh
@@ -27,6 +27,3 @@ echo "######"
 while read p; do
   echo >> expiry.csv && echo  $(hostname && echo ',' && echo `date --date="$(openssl x509 -in $p -noout -enddate | cut -d= -f 2)" --iso-8601` ; echo "," ;echo `openssl x509 -in $p -text | grep DNS` ; echo ","; echo "$p") |tr '\n' '\t' >> expiry.csv
 done <list30.txt
-
-
-#openssl req -x509 -newkey rsa:4096 -sha256 -days 25 -nodes   -keyout example.key -out 25.crt -subj "/CN=example.com"   -addext "subjectAltName=DNS:example.com,DNS:www.example.net,IP:10.0.0.1"
